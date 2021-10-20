e42b74a830dc75f79550ebfc8e6c9427fa6c10e6
katsu0120 <hayakatsu364@gmail.com>
2021-10-20 23:23:52 +0900

    83/Add auth token controller test

--- app/controllers/api/v1/auth_token_controller.rb
+++ app/controllers/api/v1/auth_token_controller.rb
@@ -23,7 +23,7 @@ class Api::V1::AuthTokenController < ApplicationController
   # リフレッシュ
   def refresh
     @user = session_user
-    set_refresh_token_to_cookie
+    # set_refresh_token_to_cookie
     render json: login_response
   end
 
